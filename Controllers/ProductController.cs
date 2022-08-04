48fb6b55658a1245598b1e48368d45ee68c14379
rodrigomouraf <rodrigomoura_rm@hotmail.com>
2022-08-04 18:23:07 -0300

    feat: inserido controller para produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get
+        (
+            [FromServices] DataContext context
+        )
+        {
+            try
+            {
+                var products = await context
+                    .Products
+                    .Include(c => c.Category)
+                    .AsNoTracking()
+                    .ToListAsync();
+
+                return Ok(products);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível listar os produtos." });
+            }
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public async Task<ActionResult<Product>> Get(
+            int id
+            ,[FromServices] DataContext context
+        )
+        {
+            try
+            {
+                var product = await context
+                    .Products
+                    .Include(c => c.Category)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.Id == id);
+
+                if (product?.Id != id)
+                {
+                    return NotFound(new { message = "Produto não encontrado" });
+                }
+
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível listar os produtos" });
+            }
+        }
+
+        [Route("categories/{id:int}")]
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetByCategory(
+            int id,
+            [FromServices] DataContext context
+        )
+        {
+            try
+            {
+                var product = await context
+                    .Products
+                    .Include(c => c.Category)
+                    .AsNoTracking()
+                    .Where(c => c.CategoryId == id)
+                    .ToListAsync();
+
+                if (product.Count == 0)
+                {
+                    return NotFound(new { message = "Não existem produtos para essa categoria." });
+                }
+
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível listar os produtos para essa categoria" });
+            }
+        }
+
+        [Route("")]
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post(
+        [FromBody] Product model,
+        [FromServices] DataContext context
+    )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possível criar categoria" });
+            }
+        }
+    }
+}
